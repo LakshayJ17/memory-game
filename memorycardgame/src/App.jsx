2f49e84b4b6914d051a7cac6520d5a08788c9f86
lakshay17 <lakshayjain01lj@gmail.com>
2025-01-27 22:28:32 +0530

    add axios for API calls and implement Pokémon data fetching and shuffling

--- memorycardgame/src/App.jsx
+++ memorycardgame/src/App.jsx
@@ -1,23 +1,62 @@
 import './App.css'
 import Card from './components/Card'
 import Header from './components/Header'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [currentScore, setCurrentScore] = useState(0)
+  const [highestScore, setHighestScore] = useState(0)
+  const [pokemonData, setPokemonData] = useState([])
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        // Fetching data from the API
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=6')
+        const pokemonList = response.data.results;
+
+        // Fetching details for each pokemon
+        const details = await Promise.all(
+          pokemonList.map((pokemon) => axios.get(pokemon.url))
+        );
+
+        // Formatting the data
+        const formattedData = details.map((res) => ({
+          name: res.data.name,
+          image: res.data.sprites.front_default,
+        }));
+
+        shuffleCards(formattedData);
+      } catch (error) {
+        console.log("error faced fetching pokemon" + error)
+      }
+    }
+
+    fetchPokemon()
+  }, [])
+
+  const shuffleCards = (data) => {
+    const shuffled = (data || pokemonData).sort(() => Math.random() - 0.5);
+    setPokemonData([...shuffled]);
+  };
+
 
   return (
     <>
-      <Header />
-      <div className='grid grid-cols-4 gap-4 p-10'>
-        <Card imageSrc="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" name="Animal" />
-        <Card imageSrc="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" name="Animal" />
-        <Card imageSrc="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" name="Animal" />
-        <Card imageSrc="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" name="Animal" />
-        <Card imageSrc="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" name="Animal" />
-        <Card imageSrc="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" name="Animal" />
-        <Card imageSrc="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" name="Animal" />
+      <Header currentScore={0} highestScore={0} />
+      <div className='grid grid-cols-3 gap-4 p-10'>
+        {pokemonData.map((pokemon, index) => (
+          <Card 
+          key={index} 
+          imageSrc={pokemon.image} 
+          name={pokemon.name} 
+          onClick={() => shuffleCards()}
+          />
+        ))}
       </div>
 
-
+ 
     </>
   )
 }
--- memorycardgame/src/components/Card.jsx
+++ memorycardgame/src/components/Card.jsx
@@ -13,12 +13,13 @@
 
 import React from 'react';
 
-function Card({ imageSrc, name }) {
+function Card({ imageSrc, name, onClick }) {
   return (
     <div className="border rounded-lg shadow-md p-4 text-center hover:shadow-lg transition-shadow max-w-100 m-5">
       <img
         src={imageSrc}
         alt={name}
+        onClick={onClick}
         className="w-full h-48 object-cover rounded-md mb-2"
       />
       <h1 className="text-lg font-bold text-gray-800">{name}</h1>
